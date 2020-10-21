7f1d619696e4377cdf5efd814570d4c7ace5e4cd
esmaeil bahrani fard <esmaeil94@gmail.com>
2020-10-22 01:36:51 +0330

    Manage users

---
+++ model/paperUser.model.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *      ****  *  *     *  ****  ****  *    *
+ *      *  *  *  * *   *  *  *  *  *   *  *
+ *      ****  *  *  *  *  *  *  *  *    *
+ *      *     *  *   * *  *  *  *  *   *  *
+ *      *     *  *    **  ****  ****  *    *
+ * @author   Pinoox
+ * @license  https://opensource.org/licenses/MIT MIT License
+ */
+
+namespace pinoox\app\com_pinoox_paper\model;
+
+
+class PaperUserModel extends PaperDatabase
+{
+
+
+    public static function sort($sort)
+    {
+        if (!empty($sort) && isset($sort['field']) && !empty($sort['field'])) {
+            if ($sort['field'] === 'approx_register_date')
+                $sort['field'] = 'register_date';
+
+            self::$db->orderBy($sort['field'], $sort['type']);
+        }
+    }
+
+    public static function where_status($status)
+    {
+        if (!is_null($status))
+            self::$db->where('u.status', $status);
+    }
+
+}
