6aee96d90237b472f9ee040cb6a873c4778684ab
yoosef alipour <yoosefalipour@gmail.com>
2020-11-01 12:27:33 +0330

    Add change & remove avatar

--- controller/api/panel/v1/user.controller.php
+++ controller/api/panel/v1/user.controller.php
@@ -17,10 +17,12 @@ use pinoox\app\com_pinoox_paper\model\PaperUserModel;
 use pinoox\component\Pagination;
 use pinoox\component\Request;
 use pinoox\component\Response;
+use pinoox\component\Uploader;
 use pinoox\component\Url;
 use pinoox\component\User;
 use pinoox\component\Validation;
 use pinoox\model\FileModel;
+use pinoox\model\PinooxDatabase;
 use pinoox\model\UserModel;
 
 class UserController extends LoginConfiguration
@@ -35,9 +37,9 @@ class UserController extends LoginConfiguration
     {
         $user = User::get();
         return [
+            'is_avatar' => !empty($user['avatar_id']),
             'avatar' => Url::upload($user['avatar_id'], Url::file('resources/avatar.png')),
             'avatar_thumb' => Url::thumb($user['avatar_id'], 128, Url::file('resources/avatar.png')),
-            'isAvatar' => !empty($user['avatar_id']),
             'fname' => $user['fname'],
             'lname' => $user['lname'],
             'full_name' => $user['full_name'],
@@ -232,5 +234,55 @@ class UserController extends LoginConfiguration
         }
 
         Response::json(rlang('user.err_old_password'), false);
+    }
+
+    public function removeAvatar()
+    {
+        if (!User::isLoggedIn())
+            $this->error();
+        $user = User::get();
+        Uploader::init()->thumb('128f', PINOOX_PATH_THUMB)->actRemoveRow($user['avatar_id']);
+        if (UserModel::update_avatar($user['user_id'], null)) {
+            $user = $this->getUser();
+            Response::json($user, true);
+        }
+
+        Response::json(rlang('panel.error_happened'), false);
+    }
+
+    public function changeAvatar()
+    {
+        if (!User::isLoggedIn())
+            $this->error();
+
+        if (Request::isFile('avatar')) {
+
+            PinooxDatabase::startTransaction();
+
+            $old_avatar_id = User::get('avatar_id');
+            $up = Uploader::init('avatar', path('uploads/avatar/'))
+                ->allowedTypes('png,jpg,jpeg', 2)
+                ->changeName('time')
+                ->transaction()
+                ->thumb('128f,256f,512f', PINOOX_PATH_THUMB)
+                ->insert('none', 'avatar')->finish(true);
+
+            $avatar_id = $up->getInsertId();
+            if ($up->isCommit() && UserModel::update_avatar(User::get('user_id'), $avatar_id)) {
+                PinooxDatabase::commit();
+                $up->commit();
+                Uploader::init()->thumb('128f', PINOOX_PATH_THUMB)->actRemoveRow($old_avatar_id);
+            }
+            if ($result = $up->result()) {
+                Response::json([
+                    'avatar' => Url::upload($result),
+                    'avatar_thumb' => Url::thumb($result),
+                ], true);
+            } else {
+                Response::json($up->error('first'), false);
+            }
+        }
+
+        Response::json(rlang('panel.error_happened'), false);
     }
 }
--- theme/panel/papyrus/src/vue/pages/profile.vue
+++ theme/panel/papyrus/src/vue/pages/profile.vue
@@ -1,125 +1,127 @@
 <template>
-    <div class="form-wrapper">
-            <section>
-                <div class="form-content row" @keyup.enter="saveUserProfile()">
-                    <row :gutter="12" :columns="4" class="col-sm-order">
-                        <div class="header order-1" >
-                            <div class="title">
-                                <div class="text">
-                                    {{LANG.panel.user_profile}}
-                                </div>
+    <simplebar class="form-wrapper">
+        <section>
+            <div class="form-content row" @keyup.enter="saveUserProfile()">
+                <row :gutter="12" :columns="4" class="col-sm-order">
+                    <div class="header order-1">
+                        <div class="title">
+                            <div class="text">
+                                {{LANG.panel.user_profile}}
                             </div>
                         </div>
-                        <column :sm="3" :lg="2" class="order-2">
-                            <div class="input-wrapper">
-                                <label class="input-label">{{LANG.user.fname}}</label>
-                                <div class="input-group">
-                                    <input v-model="paramsUserProfile.fname" type="text"
-                                           :placeholder="LANG.user.fname" class="input">
-                                </div>
+                    </div>
+                    <column :sm="3" :lg="2" class="order-2">
+                        <div class="input-wrapper">
+                            <label class="input-label">{{LANG.user.fname}}</label>
+                            <div class="input-group">
+                                <input v-model="paramsUserProfile.fname" type="text"
+                                       :placeholder="LANG.user.fname" class="input">
                             </div>
-                            <div class="input-wrapper">
-                                <label class="input-label">{{LANG.user.lname}}</label>
-                                <div class="input-group">
-                                    <input v-model="paramsUserProfile.lname" type="text"
-                                           :placeholder="LANG.user.lname" class="input">
-                                </div>
+                        </div>
+                        <div class="input-wrapper">
+                            <label class="input-label">{{LANG.user.lname}}</label>
+                            <div class="input-group">
+                                <input v-model="paramsUserProfile.lname" type="text"
+                                       :placeholder="LANG.user.lname" class="input">
                             </div>
-                            <div class="input-wrapper">
-                                <label class="input-label">{{LANG.user.username}}</label>
-                                <div class="input-group">
-                                    <input v-model="paramsUserProfile.username" type="text"
-                                           :placeholder="LANG.user.username" class="input">
-                                </div>
+                        </div>
+                        <div class="input-wrapper">
+                            <label class="input-label">{{LANG.user.username}}</label>
+                            <div class="input-group">
+                                <input v-model="paramsUserProfile.username" type="text"
+                                       :placeholder="LANG.user.username" class="input">
                             </div>
-                            <div class="input-wrapper">
-                                <label class="input-label">{{LANG.user.email}}</label>
-                                <div class="input-group">
-                                    <input v-model="paramsUserProfile.email" type="text"
-                                           :placeholder="LANG.user.email" class="input">
-                                </div>
+                        </div>
+                        <div class="input-wrapper">
+                            <label class="input-label">{{LANG.user.email}}</label>
+                            <div class="input-group">
+                                <input v-model="paramsUserProfile.email" type="text"
+                                       :placeholder="LANG.user.email" class="input">
                             </div>
-                        </column>
-                        <column :sm="1" :lg="2" class="order-0">
-                            <div class="input-wrapper">
-                                <label class="input-label center">تصویر پروفایل</label>
-                                <div class="input-group">
-                                    <picture-input
-                                            ref="pictureInput"
-                                            width="200"
-                                            height="200"
-                                            radius="50"
-                                            accept="image/jpeg,image/png"
-                                            :hide-change-button="false"
-                                            :crop="true"
-                                            :removable="true"
-
-                                            button-class="btn btn-sm btn-primary"
-                                            remove-button-class="btn btn-sm btn-danger"
-                                            :custom-strings="{
+                        </div>
+                    </column>
+                    <column :sm="1" :lg="2" class="order-0">
+                        <div class="input-wrapper">
+                            <label class="input-label center">تصویر پروفایل</label>
+                            <div class="input-group">
+                                <picture-input
+                                        ref="pictureInput"
+                                        width="200"
+                                        height="200"
+                                        radius="50"
+                                        :toggle-aspect-ratio="true"
+                                        :auto-toggle-aspect-ratio="true"
+                                        accept="image/jpeg,image/png"
+                                        :prefill="avatar"
+                                        :hide-change-button="false"
+                                        :crop="true"
+                                        :removable="true"
+                                        @change="changeAvatar"
+                                        @remove="removeAvatar"
+                                        button-class="btn btn-sm btn-primary"
+                                        remove-button-class="btn btn-sm btn-danger"
+                                        :custom-strings="{
         upload: 'آپلود',
         drag: 'ضربه بزنید یا تصویر را بکشید و اینجا رها کنید',
         remove: 'حذف',
         select: 'انتخاب تصویر',
         change: 'تغییر تصویر',
       }">
-                                    </picture-input>
-                                </div>
-                            </div>
-                        </column>
-                        <div class="footer order-3">
-                            <div class="btn btn-primary" @click="saveUserProfile()">{{LANG.panel.save}}</div>
-                        </div>
-                    </row>
-                </div>
-                <div class="form-content row" @keyup.enter="changePassword()">
-                    <row :gutter="12" :columns="4">
-                        <div class="header">
-                            <div class="title">
-                                <div class="text">
-                                    {{LANG.user.change_password}}
-                                </div>
+
+                                </picture-input>
                             </div>
                         </div>
-                        <column :sm="3" :lg="2">
-                            <div class="input-wrapper">
-                                <label class="input-label">{{LANG.user.old_password}}</label>
-                                <div class="input-group">
-                                    <input v-model="paramsChangePassword.old_password" type="password"
-                                           :placeholder="LANG.user.enter_old_password" class="input">
-                                </div>
+                    </column>
+                    <div class="footer order-3">
+                        <div class="btn btn-primary" @click="saveUserProfile()">{{LANG.panel.save}}</div>
+                    </div>
+                </row>
+            </div>
+            <div class="form-content row" @keyup.enter="changePassword()">
+                <row :gutter="12" :columns="4">
+                    <div class="header">
+                        <div class="title">
+                            <div class="text">
+                                {{LANG.user.change_password}}
                             </div>
-                            <div class="input-wrapper">
-                                <label class="input-label">{{LANG.user.new_password}}</label>
-                                <div class="input-group">
-                                    <input v-model="paramsChangePassword.password" type="password"
-                                           :placeholder="LANG.user.enter_new_password" class="input">
-                                </div>
-                            </div>
-                            <div class="input-wrapper">
-                                <label class="input-label">{{LANG.user.re_new_password}}</label>
-                                <div class="input-group">
-                                    <input v-model="paramsChangePassword.re_password" type="password"
-                                           :placeholder="LANG.user.enter_re_new_password" class="input">
-                                </div>
-                            </div>
-                        </column>
-                        <div class="footer">
-                            <div class="btn btn-primary" @click="changePassword()">{{LANG.panel.save}}</div>
-                            <br/>
-                            <br/>
                         </div>
-                    </row>
-                </div>
-            </section>
-        </div>
+                    </div>
+                    <column :sm="3" :lg="2">
+                        <div class="input-wrapper">
+                            <label class="input-label">{{LANG.user.old_password}}</label>
+                            <div class="input-group">
+                                <input v-model="paramsChangePassword.old_password" type="password"
+                                       :placeholder="LANG.user.enter_old_password" class="input">
+                            </div>
+                        </div>
+                        <div class="input-wrapper">
+                            <label class="input-label">{{LANG.user.new_password}}</label>
+                            <div class="input-group">
+                                <input v-model="paramsChangePassword.password" type="password"
+                                       :placeholder="LANG.user.enter_new_password" class="input">
+                            </div>
+                        </div>
+                        <div class="input-wrapper">
+                            <label class="input-label">{{LANG.user.re_new_password}}</label>
+                            <div class="input-group">
+                                <input v-model="paramsChangePassword.re_password" type="password"
+                                       :placeholder="LANG.user.enter_re_new_password" class="input">
+                            </div>
+                        </div>
+                    </column>
+                    <div class="footer">
+                        <div class="btn btn-primary" @click="changePassword()">{{LANG.panel.save}}</div>
+                        <br/>
+                        <br/>
+                    </div>
+                </row>
+            </div>
+        </section>
+    </simplebar>
 </template>
 
 <script>
-    import ImageUpload from '../components/image-upload.vue';
-
     export default {
-        components:{ImageUpload},
         name: "profile",
         created() {
             this.getUserProfile();
@@ -136,7 +138,8 @@
                     password: null,
                     re_password: null,
                     old_password: null,
-                }
+                },
+                avatar: null,
             }
         },
         methods: {
@@ -152,8 +155,35 @@
             },
             changePassword() {
                 this.$http.post(this.URL.API + 'user/changePassword', this.paramsChangePassword).then((json) => {
-                    if(this._statusResponse(json.data))
+                    if (this._statusResponse(json.data))
                         this._resetInitialData('paramsChangePassword');
+                });
+            },
+            changeAvatar(file) {
+                let data = new FormData();
+                data.append('avatar', this.$refs.pictureInput.file);
+                this.$http.post(this.URL.API + 'user/changeAvatar/', data).then((json) => {
+                    if (json.data.status) {
+                        this.USER.avatar = json.data.result.avatar;
+                        this.USER.avatar_thumb = json.data.result.avatar_thumb;
+                        this.USER.is_avatar = true;
+                    } else {
+                        this._notify('error', json.data.result);
+                    }
+                })
+            },
+            removeAvatar() {
+                if (!this.USER.is_avatar)
+                    return;
+                this.$http.get(this.URL.API + 'user/removeAvatar/').then((json) => {
+                    if (json.data.status) {
+                        this.avatar = null;
+                        this.USER.avatar = json.data.result.avatar;
+                        this.USER.avatar_thumb = json.data.result.avatar_thumb;
+                        this.USER.is_avatar = false;
+                    } else {
+                        this._notify('error', json.data.result);
+                    }
                 });
             },
             getUserProfile() {
@@ -161,6 +191,9 @@
                     ...this.paramsUserProfile,
                     ...this.USER,
                 };
+
+                if (this.USER.is_avatar)
+                    this.avatar = this.USER.avatar;
             },
             setUserProfile() {
                 this.USER = {
