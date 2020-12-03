eaab070316d1a2e9151a25b07b70a381811f0026
esmaeil bahrani fard <esmaeil94@gmail.com>
2020-12-03 19:28:46 +0330

    Set toranj as default paper template

--- controller/panel/api.controller.php
+++ controller/panel/api.controller.php
@@ -10,17 +10,9 @@
  */
 namespace pinoox\app\com_pinoox_paper\controller\panel;
 
-use pinoox\app\com_pinoox_paper\component\Setting;
-use pinoox\app\com_pinoox_paper\model\CommentModel;
-use pinoox\app\com_pinoox_paper\model\ContactModel;
-use pinoox\app\com_pinoox_paper\model\LangModel;
 use pinoox\component\Dir;
 use pinoox\component\HelperHeader;
-use pinoox\component\Lang;
-use pinoox\component\Request;
-use pinoox\component\Response;
 use pinoox\component\Router;
-use pinoox\model\UserModel;
 
 class MainController extends MasterConfiguration
 {
