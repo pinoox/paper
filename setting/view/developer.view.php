e4b6f7261f0df3fb2ba461237373e56d6ad76743
yoosef alipour <yoosefalipour@gmail.com>
2021-01-29 20:30:59 +0330

    npm run release

--- setting/view/developer.view.php
+++ setting/view/developer.view.php
@@ -7,60 +7,6 @@ return [
     'sort' => 10,
     'settings' => [
         [
-            'key' => 'hook',
-            'label' => rlang('setting.web_hook'),
-            'type' => 'form',
-            'settings' => [
-                [
-                    'key' => 'address',
-                    'label' => rlang('setting.address'),
-                    'type' => 'text',
-                ],
-                [
-                    'key' => 'token',
-                    'type' => 'text',
-                    'label' => rlang('setting.access_key'),
-                ],
-            ],
-            'value' => [
-                'address' => '',
-                'key' => 'pinoox_adasFJAGdnquin32e4893',
-            ],
-        ],
-        [
-            'key' => 'ftp',
-            'label' => rlang('setting.ftp'),
-            'type' => 'form',
-            'settings' => [
-                [
-                    'key' => 'address',
-                    'label' => rlang('setting.address'),
-                    'type' => 'text',
-                ],
-                [
-                    'key' => 'host',
-                    'type' => 'text',
-                    'label' => rlang('setting.ftp_host'),
-                ],
-                [
-                    'key' => 'username',
-                    'type' => 'text',
-                    'label' => rlang('setting.username'),
-                ],
-                [
-                    'key' => 'password',
-                    'type' => 'text',
-                    'label' => rlang('setting.password'),
-                ],
-            ],
-            'value' => [
-                'address' => '',
-                'host' => '',
-                'username' => '',
-                'password' => '',
-            ],
-        ],
-        [
             'key' => 'header_code',
             'label' => rlang('setting.header_code'),
             'type' => 'textarea',
