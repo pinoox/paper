bb17a85b38590c5504641dca718a6107c31a7fd7
yoosef alipour <yoosefalipour@gmail.com>
2021-11-09 21:39:07 +0330

    Fix image caption in Paper editor

--- theme/panel/papyrus/src/vue/components/editor.vue
+++ theme/panel/papyrus/src/vue/components/editor.vue
@@ -159,6 +159,7 @@ export default {
           toolbar: [
             'linkImage',
             'imageTextAlternative',
+            'toggleImageCaption',
             '|',
             'imageStyle:alignRight',
             'imageStyle:alignCenter',
